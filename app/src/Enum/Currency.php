6eacd651a3fdcd1f07aef640421a4d95056c53c6
Norivo7 <szymik.kamil97@gmail.com>
2026-03-08 21:25:46 +0100

    feat: add values() helper function for enums

--- app/src/Enum/Currency.php
+++ app/src/Enum/Currency.php
@@ -9,4 +9,9 @@ enum Currency: string
     case PLN = 'PLN';
     case EUR = 'EUR';
     case USD = 'USD';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
--- app/src/Enum/ProductStatus.php
+++ app/src/Enum/ProductStatus.php
@@ -9,4 +9,9 @@ enum ProductStatus: string
     case ACTIVE = 'active';
     case INACTIVE = 'inactive';
     case DRAFT = 'draft';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
